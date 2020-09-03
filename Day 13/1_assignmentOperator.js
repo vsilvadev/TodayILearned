844f72efe26d00bfd930210d7a96e854dc2cbb59
Vitor Silva <vitorabsilva10@gmail.com>
2020-09-03 19:48:15 -0300

    Add Day 13 Folder

---
+++ Day 13/1_assignmentOperator.js
@@ -0,0 +1,17 @@
+const a = 7; //Assigning a value of 7 to the Var
+let b = 3; //Assigning a value of 3 to the Let
+
+b += a; //b = b + a
+console.log(b);
+
+b -= 4; //b = b - 4
+console.log(b);
+
+b *= 2; //b = b * 2
+console.log(b);
+
+b /= 2; //b = b / 2
+console.log(b);
+
+b %= 2; //b = b % 2
+console.log(b);
