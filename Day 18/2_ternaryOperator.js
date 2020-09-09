6d51e206b4afe227b72414b3a6d996fb3f739212
Vitor Silva <vitorabsilva10@gmail.com>
2020-09-08 21:02:06 -0300

    Add File about Ternary Operator

---
+++ Day 18/2_ternaryOperator.js
@@ -0,0 +1,4 @@
+const result = grade => grade >= 7 ? "Approved" : "Disapproved"; //Expression to see if's true or false ? Return True : Return False
+
+console.log(result(7.1));
+console.log(result(6.7));
